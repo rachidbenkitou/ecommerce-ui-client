8ab27d9b9d2fd1d1f1b45e7d0441ff2016f43e1c
Rachid Benkitou <rachidbenkitou2021@gmail.com>
2024-03-11 11:51:22 +0000

    working in landing page

--- src/app/developments/shared/components/navbar/navbar.component.ts
+++ src/app/developments/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ProductsService} from "../../../products/services/products.service";
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  productCount: number=0;
+  constructor(private productService: ProductsService) {
+    this.productService.productCount$.subscribe(count => {
+      this.productCount = count;
+      // Update cart component or perform any other actions
+    });
+  }
 }
