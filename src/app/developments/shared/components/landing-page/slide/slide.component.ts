d6b47ef084e883f1dcf04746f6b2c535b3550761
Rachid Benkitou <rachidbenkitou2021@gmail.com>
2024-04-15 04:17:33 +0100

    multi slider page

--- src/app/developments/shared/components/landing-page/slide/slide.component.ts
+++ src/app/developments/shared/components/landing-page/slide/slide.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+//
+// @Component({
+//   selector: 'app-slide',
+//   templateUrl: './slide.component.html',
+//   styleUrls: ['./slide.component.scss'] // Make sure you have a corresponding SCSS file if you want to add styles
+// })
+// export class SlideComponent implements OnInit {
+//   slides: any[] = [
+//     {
+//       subtitle: 'Hot promotions',
+//       title: 'Fashion Trending Great Collection',
+//       description: 'Save more with coupons & up to 20% off',
+//       buttonLink: 'shop.html',
+//       buttonText: 'Shop Now'
+//     },
+//     {
+//       subtitle: 'Another slide',
+//       title: 'Another Title',
+//       description: 'Another description for the slide',
+//       buttonLink: 'another-page.html',
+//       buttonText: 'Go to Another Page'
+//     }
+//   ];
+//
+//   currentSlideIndex: number = 0;
+//   currentSlide: any;
+//
+//   constructor() { }
+//
+//   ngOnInit(): void {
+//     this.startSlider();
+//   }
+//
+//   startSlider() {
+//     setInterval(() => {
+//       this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length;
+//       this.currentSlide = this.slides[this.currentSlideIndex];
+//     }, 5000);
+//   }
+// }
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slide',
   templateUrl: './slide.component.html',
-  styleUrl: './slide.component.scss'
+  styleUrls: ['./slide.component.scss']
 })
-export class SlideComponent {
+export class SlideComponent implements OnInit {
+  slides: any[] = [
+    {
+      subtitle: 'Hot promotions',
+      title: 'Fashion Trending Great Collection',
+      description: 'Save more with coupons & up to 20% off',
+      buttonLink: 'shop.html',
+      buttonText: 'Shop Now'
+    },
+    {
+      subtitle: 'Another slide',
+      title: 'Another Title',
+      description: 'Another description for the slide',
+      buttonLink: 'another-page.html',
+      buttonText: 'Go to Another Page'
+    }
+  ];
 
+  currentSlideIndex: number = 0;
+  currentSlide: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.startSlider();
+  }
+
+  startSlider() {
+    this.currentSlide = this.slides[this.currentSlideIndex];
+    setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
+
+  prevSlide() {
+    this.currentSlideIndex = (this.currentSlideIndex - 1 + this.slides.length) % this.slides.length;
+    this.currentSlide = this.slides[this.currentSlideIndex];
+  }
+
+  nextSlide() {
+    this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length;
+    this.currentSlide = this.slides[this.currentSlideIndex];
+  }
 }
--- src/app/developments/shared/shared.module.ts
+++ src/app/developments/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from "@angular/core";
 import {BreadcrumbComponent} from './components/breadcrumb/breadcrumb.component';
 import {NotFoundComponent} from './components/not-found/not-found.component';
-import { DealsComponent } from './components/landing-page/deals/deals.component';
-import { SlideComponent } from './components/landing-page/slide/slide.component';
+import {DealsComponent} from './components/landing-page/deals/deals.component';
+import {SlideComponent} from './components/landing-page/slide/slide.component';
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -11,11 +12,12 @@ import { SlideComponent } from './components/landing-page/slide/slide.component'
     DealsComponent,
     SlideComponent,
   ],
-    exports: [
-        BreadcrumbComponent,
-        DealsComponent,
-        SlideComponent,
-    ],
+  exports: [
+    BreadcrumbComponent,
+    DealsComponent,
+    SlideComponent,
+    CommonModule
+  ],
   imports: []
 })
 
